956e8b4ff98a1900f1cac5c25c03fe7062117101
nightost <nightost@yeah.net>
2016-04-13 15:33:31 +0800

    init page structrue
    init page structrue

---
+++ sayHello.js
@@ -0,0 +1,13 @@
+/**
+ * Created by nightost on 16/4/13.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!');
+});
